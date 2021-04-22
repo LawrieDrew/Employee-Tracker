4fdb430efa2a501c38fc34a1334d6bc1c8d7b66a
LawrieDrew <laurenn.drew@gmail.com>
2021-04-22 12:54:25 -0400

    pseudo code for js and sql

---
+++ tracker.js
@@ -0,0 +1,13 @@
+//dependencies
+
+//connections
+
+//make a list w/switch statement like Corey taught you
+//what would you like to do?
+//View all employees
+//View all departments
+//View all roles
+//Search for ee
+//search for ee by manager
+//remove ee
+//remove department 
