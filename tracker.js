2fc95a3fefaa31eb5bd4d0f293fea2317d502d81
LawrieDrew <laurenn.drew@gmail.com>
2021-04-22 13:06:29 -0400

    set up dependencies and made sql connection

--- tracker.js
+++ tracker.js
@@ -1,6 +1,26 @@
 //dependencies
 
+const mysql = require('mysql');
+const pass = require('./config.js');
+
 //connections
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'trackerDB',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('connected as id ${connection.threadID}');
+    connection.end();
+
+
+
+})
 
 //make a list w/switch statement like Corey taught you
 //what would you like to do?
@@ -11,3 +31,4 @@
 //search for ee by manager
 //remove ee
 //remove department 
+
